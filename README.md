d9d63837d0db8087f4a8ec0d3086f3081018bbd5
hyorn <jiangqi@cnipa.gov.cn>
2019-11-03 00:00:00 +0800

    cnipa_springBoot-fm

---
+++ README.md
@@ -0,0 +1,2 @@
+# cnipa-fm
+基于springBoot财务系统 国家知识产权局http://39.103.176.146/
---
+++ src/test/java/com/cnipa/fm/FmApplicationTests.java
@@ -0,0 +1,22 @@
+package com.yuxin.fm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *    国家知识产权局
+ *    待上线域名cnipa.gov.cn
+ *    http://39.103.176.146/  admin:cnipa
+ *    jiangqi@cnipa.gov.cn
+ * */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
